c34e7eac10f4703fb7138da2cc7af8ec7901cdad
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-10 23:04:11 +0700

    update base views

---
+++ suitmedia_intern_flutter_application_mobile/lib/views/first_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstScreenViews extends StatefulWidget {
+  const FirstScreenViews({super.key});
+
+  @override
+  State<FirstScreenViews> createState() => _FirstScreenViewsState();
+}
+
+class _FirstScreenViewsState extends State<FirstScreenViews> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ suitmedia_intern_flutter_application_mobile/lib/views/second_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondScreenViews extends StatelessWidget {
+  const SecondScreenViews({super.key, required String userName});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
