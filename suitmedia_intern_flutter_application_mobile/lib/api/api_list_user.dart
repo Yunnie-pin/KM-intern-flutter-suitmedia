eca4035842003ba177b66d68caf473e2522df493
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-10 22:30:37 +0700

    menambahkan model dan api service untuk list user

---
+++ suitmedia_intern_flutter_application_mobile/lib/api/api_list_user.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:suitmedia_intern_flutter_application_mobile/model/user_list.dart';
+
+class ApiService {
+  static const String _baseUrl = 'https://reqres.in/api/users?';
+  static const int _perPage = 6;
+
+  Future<UserList> getUserList(http.Client client, int page) async {
+    final response = await client.get(Uri.parse('$_baseUrl' 'page=$page&per_page=$_perPage'));
+    if (response.statusCode == 200) {
+      return UserList.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load user list');
+    }
+  }
+}
