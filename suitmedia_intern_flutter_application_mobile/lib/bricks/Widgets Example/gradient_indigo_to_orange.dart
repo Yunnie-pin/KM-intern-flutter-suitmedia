abb52a26804ee013ef1e057e73daa21a6a2cbc78
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-11 00:26:02 +0700

    update static first screen views

---
+++ suitmedia_intern_flutter_application_mobile/lib/bricks/Widgets Example/gradient_indigo_to_orange.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+gradient6(){
+return const LinearGradient(colors: [Colors.indigo, Colors.orange]);
+}
--- suitmedia_intern_flutter_application_mobile/lib/views/first_screen.dart
+++ suitmedia_intern_flutter_application_mobile/lib/views/first_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FirstScreenViews extends StatefulWidget {
-  const FirstScreenViews({super.key});
+  const FirstScreenViews({Key? key}) : super(key: key);
 
   @override
   State<FirstScreenViews> createState() => _FirstScreenViewsState();
@@ -10,6 +10,109 @@ class FirstScreenViews extends StatefulWidget {
 class _FirstScreenViewsState extends State<FirstScreenViews> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 101, 122, 245),
+          Color.fromARGB(255, 87, 93, 131)
+        ])),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black26,
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Icon(
+                      Icons.group_add_rounded,
+                      color: Colors.white,
+                      size: 80,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    contentPadding: EdgeInsets.only(
+                        left: 16, right: 16, top: 12, bottom: 12),
+                    hintText: 'Name',
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    isDense: true,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    contentPadding: EdgeInsets.only(
+                        left: 16, right: 16, top: 12, bottom: 12),
+                    hintText: 'Name',
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    isDense: true,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onPressed: null,
+                  child: const Text(
+                    'Check',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onPressed: null,
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
