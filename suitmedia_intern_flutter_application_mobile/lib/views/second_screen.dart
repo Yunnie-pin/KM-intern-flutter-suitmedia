3cb7c6a5bb0f583ef6e62643a35ec48465382971
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-11 09:06:21 +0700

    update second screend

--- suitmedia_intern_flutter_application_mobile/lib/views/second_screen.dart
+++ suitmedia_intern_flutter_application_mobile/lib/views/second_screen.dart
@@ -6,8 +6,62 @@ class SecondScreenViews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Value: $userName'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.black12,
+            height: 2.0,
+          ),
+        ),
+        title: const Text(
+          'Second Screen',
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              userName,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+            )
+          ],
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 31, 63, 112),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/third');
+          },
+          child: const Text(
+            'Choose a User',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- suitmedia_intern_flutter_application_mobile/lib/views/third_screen.dart
+++ suitmedia_intern_flutter_application_mobile/lib/views/third_screen.dart
@@ -5,6 +5,8 @@ class ThirdScreenViews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text('Third Screen'),
+    );
   }
 }
